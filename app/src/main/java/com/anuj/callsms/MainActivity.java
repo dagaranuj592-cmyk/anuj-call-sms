962621308f3128aecfe2ca3a5e75017f6853527c
dagaranuj592-cmyk <dagaranuj592@gmail.com>
2026-08-20 18:02:05 +0530

    Update MainActivity.java

--- app/src/main/java/com/anuj/callsms/MainActivity.java
+++ app/src/main/java/com/anuj/callsms/MainActivity.java
@@ -2,7 +2,6 @@ package com.anuj.callsms;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,11 +9,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView text = new TextView(this);
-        text.setText("Anuj Confectionary\n\nCall ke baad automatic SMS system");
-        text.setTextSize(22);
-        text.setPadding(30, 50, 30, 30);
-
-        setContentView(text);
+        setContentView(R.layout.activity_main);
     }
 }
