85faa4674930b6a6219d3bd882bc513cf510578a
dagaranuj592-cmyk <dagaranuj592@gmail.com>
2026-08-20 20:28:55 +0530

    Update MainActivity.java

--- app/src/main/java/com/anuj/callsms/MainActivity.java
+++ app/src/main/java/com/anuj/callsms/MainActivity.java
@@ -1,14 +1,28 @@
 package com.anuj.callsms;
 
+import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends Activity {
+
+    private static final int PERMISSION_REQUEST = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(
+                new String[]{
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.SEND_SMS
+                },
+                PERMISSION_REQUEST
+            );
+        }
     }
 }
