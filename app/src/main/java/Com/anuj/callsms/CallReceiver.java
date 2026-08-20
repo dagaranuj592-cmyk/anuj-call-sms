35149550ed8d9f4d4f45c8730812e6e2a3f9faf6
dagaranuj592-cmyk <dagaranuj592@gmail.com>
2026-08-20 16:09:06 +0530

    Create CallReceiver.java

---
+++ app/src/main/java/Com/anuj/callsms/CallReceiver.java
@@ -0,0 +1,13 @@
+package com.anuj.callsms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CallReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Call receiver
+    }
+}
